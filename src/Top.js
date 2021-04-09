2b811f7778272555be5471a568e963c46502ee64
Vinit Gupta <vinitman35@gmail.com>
2021-04-09 13:21:45 +0530

    Add title and favicon and cta in top for channel

--- src/Top.js
+++ src/Top.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Reading from "./reading.svg";
+import YouTubeIcon from '@material-ui/icons/YouTube';
 import "./css/Top.css"
 
 function Main() {
@@ -8,6 +9,9 @@ function Main() {
             <div className="top__left">
                 <div className="top__left__mainHead">We talk to inform!</div>
                 <div className="top__left__subHead">Not to JUDGE...</div>
+                <div className="top__cta">
+                    <div className="cta cta__youtube"><YouTubeIcon   fontSize="large" className="social__icon youtube"/>Go to Channel</div> 
+                </div>
             </div>
             <div className="top__right">
                 <img className="top__right__image" alt="person reading blogs and news" src={Reading}></img>
