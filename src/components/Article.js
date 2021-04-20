29567badd2667541e1dd8747cda61ec5e38861f2
Vinit Gupta <vinitman35@gmail.com>
2021-04-20 12:34:02 +0530

    Add main image and body iterator to print the whole article and also embed related youtube video

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,10 +3,12 @@ import {useLocation, useParams, withRouter} from "react-router-dom"
 import firebase from "../config/firebase";
 import 'firebase/firestore';
 import parse from "html-react-parser";
+import "../css/Article.css"
 
 function Article() {
     const [article,setArticle] = useState([]);
     const [isLoaded,setIsLoaded] = useState(false);
+    const date = new Date(article?.time);
     let {id} = useParams();
     let {state} = useLocation();
     useEffect(()=>{
@@ -39,14 +41,25 @@ function Article() {
             isLoaded && 
             <div className="article">
             <div className="article__main">
-            <img src={article.imageUrl} alt="head"></img>
-            <h1>{article.title}</h1>
+            <div className="article__main__image">
+                <img src={article.imageUrl} alt="head"></img>
+            </div>
+            <div className="article__main__title">
+                <h1>{article.title}</h1>
+                <div className="article__main__date">
+                    <span><strong>Posted on : </strong>{`${date.getDate()} ${date.toLocaleString('default', { month: 'long' })}, ${date.getFullYear()}`}</span>
+                </div>
+            </div>
+            
             </div>
             <div className="article__body">
             {article?.body.length>0 && article?.body?.map(
                 (bodyVal,index) => {return parse(`<div>${bodyVal.head}${bodyVal.body}</div>`)
                 }
             )}
+            </div>
+            <div className="article__video">
+            <iframe width="720" height="405" src={article?.video} title="YouTube video player" frameborder="0" allow="autoplay" allowfullscreen></iframe>
             </div>
             </div>
     )
