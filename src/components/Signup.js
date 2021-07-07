c65c2e99c592babef1567759f5be2abe836a19fc
Vinit Gupta <vinitman35@gmail.com>
2021-07-07 20:52:50 +0530

    remove warnings in signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -49,22 +49,22 @@ function Signup() {
   .signInWithPopup(provider)
   .then((result) => {
     /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
+    //var credential = result.credential;
 
     // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = credential.accessToken;
+    // var token = credential.accessToken;
     // The signed-in user info.
     var user = result.user;
     console.log(user);
     // ...
   }).catch((error) => {
     // Handle Errors here.
-    var errorCode = error.code;
+    // var errorCode = error.code;
     var errorMessage = error.message;
     // The email of the user's account used.
-    var email = error.email;
+    // var email = error.email;
     // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
+    // var credential = error.credential;
     console.error(errorMessage)
     // ...
   });
