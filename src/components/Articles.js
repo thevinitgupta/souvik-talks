dd01bb7d54d59a84169c28609aab0ad025605251
Vinit Gupta <vinitman35@gmail.com>
2021-08-07 20:35:27 +0530

    redesign articles page

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import firebase from "../config/firebase";
 import 'firebase/firestore';
-import { Link } from 'react-router-dom';
 import "../css/Articles.css"
+import StoryCard from './StoryCard';
 
 function Articles() {
     const [articles,setArticles] = useState([]);
@@ -37,19 +37,10 @@ function Articles() {
             <div className="articles__container">
             <span className="articles__container__head">All Articles</span>
               {articles.map((article,index)=>
-                (<Link to={{
-                    pathname :`/article/${article?.id}`,
-                    state : {article:article?.data}
-                    }} 
-                   key={index+1} className="articles__container__link">
-                      <div className="articles__container__link__image">
-                        {article?.data?.imageUrl && <img src={article.data.imageUrl} alt="article"/>}
-                      </div>
-                      <div className="articles__container__link__text">
-                        <span>{article?.data?.title}</span>
-                      </div>
-                 </Link>)
-              )}
+                {
+                  return <StoryCard key={index+1} article={article}/>
+                })
+              }
             </div>
         </div>
     )
