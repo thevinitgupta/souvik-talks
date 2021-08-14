208122487e7ed9e022753335fcc789341cf7cb46
Vinit Gupta <vinitman35@gmail.com>
2021-08-14 20:51:31 +0530

    contionally display add post and add user buttons

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import NoteAddIcon from '@material-ui/icons/NoteAdd';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import "../css/ActionButton.css"
+import { AuthContext } from '../context/userContext';
 
 function ActionButton() {
+    const {user} = useContext(AuthContext);
+    const [loggedIn,setLoggedIn] = useState(false);
+    useEffect(() => {
+        console.log(user);
+        if(user) setLoggedIn(true);
+        else setLoggedIn(false);
+    }, [user])
     return (
-        <div className="actionButton">
-            <NoteAddIcon  fontSize="large" className="actionButton__icon add__post"/>
-            <PersonAddIcon  fontSize="large" className="actionButton__icon add__user"/>
+        <div className={loggedIn? "actionButton add__post" : "actionButton add__user"}>
+            {
+                loggedIn ?
+                
+                <NoteAddIcon onClick={()=>{
+                    window.location = "/post"
+                }} 
+                fontSize="large" className="actionButton__icon"/>
+                :
+                <PersonAddIcon onClick={()=>{
+                    window.location = "/login"
+                }}
+                 fontSize="large" className="actionButton__icon"/>
+            }
         </div>
     )
 }
