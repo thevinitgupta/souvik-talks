081ac02746f7356d3b81d9ece2d339bb2e331d2e
Vinit Gupta <vinitman35@gmail.com>
2021-08-16 11:20:39 +0530

    hide API keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBXf2V6ZHYXTxvVFp_gzbYH9wBvlG805MM",
-    authDomain: "souviktalks-2021.firebaseapp.com",
-    projectId: "souviktalks-2021",
-    storageBucket: "souviktalks-2021.appspot.com",
-    messagingSenderId: "834885789556",
-    appId: "1:834885789556:web:1ea16e4d8f9fce19fa2e21",
-    measurementId: "G-JXHSFQ56M3"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
   firebase.initializeApp(firebaseConfig);
